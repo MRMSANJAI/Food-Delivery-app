96e2eeb85808a7a972c621a295137d870501b035
MRMSANJAI <mrmsanjai@gmail.com>
2024-05-12 20:00:18 +0530

    commit

--- Admin/src/components/Sidebar/Sidebar.jsx
+++ Admin/src/components/Sidebar/Sidebar.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
-import './Sidebar.css'
-
+import React from 'react';
+import './Sidebar.css';
+import { assets } from '../../assets/assets';
+import {NavLink} from 'react-router-dom';
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div className='sidebar'>
+      <div className="side-options">
+        <NavLink to= '/add'className="side-option">
+           <img src={assets.add_icon} alt=""  />
+           <p>Add Items</p>
+        </NavLink>
+        <NavLink to='/list' className="side-option">
+           <img src={assets.order_icon} alt=""  />
+           <p>List Items</p>
+        </NavLink> 
+        <NavLink to='/order' className="side-option">
+           <img src={assets.order_icon} alt=""  />
+           <p>Orders</p>
+        </NavLink> 
+      </div>
+    </div>
   )
 }
 
-export default Sidebar
+export default Sidebar;
