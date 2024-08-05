25fa33098f7d6cd6902fb499ee2ad5b903f8e316
MRMSANJAI <mrmsanjai@gmail.com>
2024-08-05 22:15:52 +0530

    todays

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import { connectDB } from './config/db.js';
 import foodRouter from './routes/foodRoute.js';
 import userrouter from './routes/Userroute.js';
 import 'dotenv/config.js'
+import cartRouter from './routes/CartRoute.js';
 
 
 
@@ -23,6 +24,7 @@ connectDB();
 app.use("/api/food",foodRouter)
 app.use("/images",express.static('uploads'))
 app.use("/api/user",userrouter)
+app.use("api/cart",cartRouter)
 
 
 app.get("/",(req , res)=>{
